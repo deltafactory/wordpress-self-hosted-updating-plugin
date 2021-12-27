ef94dea2f4d5b75e866ed1214a8a68c7942aac06
Jeff Brand <jeff@deltafactory.com>
2021-12-27 16:11:05 -0500

    Add assets.

--- wordpress-self-hosted-updating-plugin.php
+++ wordpress-self-hosted-updating-plugin.php
@@ -1,5 +1,4 @@
 <?php
-	
 /**
  * Plugin Name: Self-hosted Updating Plugin Demo
  * Plugin URI: https://TBD/
@@ -7,6 +6,28 @@
  * Version: 1.0
  * Author: Jeff Brand, Delta Factory
  * Author URI: https://deltafactory.com/
- * Update URI: https://
+ * Update URI: https://raw.githubusercontent.com/deltafactory/wordpress-self-hosted-updating-plugin/master/update.json
  */
 
+namespace DeltaFactory;
+
+class Loader {
+    static function setup() {
+        add_filter( 'update_plugins_raw.githubusercontent.com', array( __CLASS__, 'update_plugin_data' ), 10, 4 );
+    }
+
+    static function update_plugin_data( $update, $plugin_data, $plugin_file, $locales ) {
+        // Is this filter running for this plugin?
+        if ( __FILE__ === $plugin_file ) {
+            $update_url = $plugin_data['UpdateURI'];
+
+            // This assumes that the data matches the format specified in https://developer.wordpress.org/reference/hooks/update_plugins_hostname/
+            $update = json_decode( wp_remote_get( $update_url ) );
+        }
+
+        return $update;
+    }
+
+}
+
+Loader::setup();

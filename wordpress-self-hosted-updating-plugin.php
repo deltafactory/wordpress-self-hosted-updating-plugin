4c43c089fa45e480887a7017c2bc107b0afda031
Jeff Brand <jeff@deltafactory.com>
2021-12-27 16:30:19 -0500

    Enable auto-update

--- wordpress-self-hosted-updating-plugin.php
+++ wordpress-self-hosted-updating-plugin.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Self-hosted Updating Plugin Demo
- * Plugin URI: https://TBD/
+ * Plugin URI: https://github.com/deltafactory/wordpress-self-hosted-updating-plugin/
  * Description: Minimal example of a plugin with update support outside of the WordPress Repository. It does nothing else.
  * Version: 1.0
  * Author: Jeff Brand, Delta Factory
@@ -18,11 +18,11 @@ class Loader {
 
     static function update_plugin_data( $update, $plugin_data, $plugin_file, $locales ) {
         // Is this filter running for this plugin?
-        if ( __FILE__ === $plugin_file ) {
+        if ( plugin_basename( __FILE__ ) === $plugin_file ) {
             $update_url = $plugin_data['UpdateURI'];
 
             // This assumes that the data matches the format specified in https://developer.wordpress.org/reference/hooks/update_plugins_hostname/
-            $update = json_decode( wp_remote_get( $update_url ) );
+            $update = json_decode( file_get_contents( $update_url ) );
         }
 
         return $update;

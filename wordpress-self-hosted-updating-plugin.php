34287cbfe977f9f976044ff143badd4dd303da42
Jeff Brand <jeff@deltafactory.com>
2021-12-27 15:48:22 -0500

    Initial commit.

---
+++ wordpress-self-hosted-updating-plugin.php
@@ -0,0 +1,12 @@
+<?php
+	
+/**
+ * Plugin Name: Self-hosted Updating Plugin Demo
+ * Plugin URI: https://TBD/
+ * Description: Minimal example of a plugin with update support outside of the WordPress Repository. It does nothing else.
+ * Version: 1.0
+ * Author: Jeff Brand, Delta Factory
+ * Author URI: https://deltafactory.com/
+ * Update URI: https://
+ */
+

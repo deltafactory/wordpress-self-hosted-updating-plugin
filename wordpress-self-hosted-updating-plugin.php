036fed3e34aa28a3a5e10efcd66510e9c56663e6
Jeff Brand <jeff@deltafactory.com>
2021-12-28 11:49:16 -0500

    Support for plugin-information pop-up, disable auto-update, and misc reorg.

--- wordpress-self-hosted-updating-plugin.php
+++ wordpress-self-hosted-updating-plugin.php
@@ -3,29 +3,34 @@
  * Plugin Name: Self-hosted Updating Plugin Demo
  * Plugin URI: https://github.com/deltafactory/wordpress-self-hosted-updating-plugin/
  * Description: Minimal example of a plugin with update support outside of the WordPress Repository. It does nothing else.
- * Version: 1.0.2
+ * Version: 1.0.4
  * Author: Jeff Brand, Delta Factory
  * Author URI: https://deltafactory.com/
  * Update URI: https://raw.githubusercontent.com/deltafactory/wordpress-self-hosted-updating-plugin/master/update.json
  */
 
-namespace DeltaFactory;
+/**
+ * Helpful resources:
+ *  - https://developer.wordpress.org/reference/hooks/update_plugins_hostname/
+ * 
+ **/
+
+namespace DeltaFactory\PluginUpdater;
+
+const PLUGIN_SLUG = 'wordpress-self-hosted-updating-plugin';
+const PLUGIN_BASENAME = 'wordpress-self-hosted-updating-plugin/wordpress-self-hosted-updating-plugin.php';
+const PLUGIN_HOSTNAME = 'raw.githubusercontent.com'; // Changes with Update URI
 
 class Loader {
-    static $plugin_basename;
 
     static function setup() {
-
-        self::$plugin_basename = plugin_basename( __FILE__ );
-
-        add_filter( 'update_plugins_raw.githubusercontent.com', array( __CLASS__, 'update_plugin_data' ), 10, 4 );
-
-        add_filter( 'plugin_row_meta', array( __CLASS__, 'plugin_meta' ), 10, 2 );
+        add_filter( 'update_plugins_' . PLUGIN_HOSTNAME, array( __CLASS__, 'update_plugin_data' ), 10, 4 );
+        add_action( 'install_plugins_pre_plugin-information', array( __CLASS__, 'details_popup' ), 9 );
     }
 
     static function update_plugin_data( $update, $plugin_data, $plugin_file, $locales ) {
         // Is this filter running for this plugin?
-        if ( self::$plugin_basename === $plugin_file ) {
+        if ( PLUGIN_BASENAME === $plugin_file ) {
             $response = wp_remote_get( $plugin_data['UpdateURI'] );
 
             // This assumes that the data matches the format specified in https://developer.wordpress.org/reference/hooks/update_plugins_hostname/
@@ -35,21 +40,22 @@ class Loader {
         return $update;
     }
 
-    static function plugin_meta( $plugin_meta, $plugin_file ) {
-        if ( self::$plugin_basename == $plugin_file ) {
-
-            // Remove last item if it's the standard TB iframe.
-            $lastitem = end( $plugin_meta );
-            if ( false !== strpos( $lastitem, 'plugin-install.php' ) ) {
-                array_pop( $plugin_meta );
-            }
-
-            // Add your own:
+    // Populate pop-up from "View Details" link on plugin/update pages.
+    static function details_popup() {
+        if ( $_REQUEST['plugin'] != PLUGIN_SLUG ) {
+            return;
         }
 
-        return $plugin_meta;
+        // There's a variation that includes &section=changelog when an update is detected.
+        /*
+        $section = !empty( $_GET['section'] ) ? $_GET['section'] : false;
+        if ( $section == 'changelog' ) {
+            ...
+        }
+        */
+        echo 'Custom content here.';
+        exit();
     }
-
 }
 
 Loader::setup();
